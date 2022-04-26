f02012e325f4320db6565d5a07d6d5c76bce8ebe
godutelaimi <79868333+godutelaimi@users.noreply.github.com>
2022-04-26 20:39:51 +0100

    Add files via upload

---
+++ SQL UPDATE.sql
@@ -0,0 +1,12 @@
+use moviecatalogue;
+
+UPDATE MOVIE 
+SET TITLE = 'Ghostbusters (1984)',
+ReleaseDate = '1984/06/08'
+WHERE MovieID = 3;
+
+UPDATE GENRE
+SET GenreName = 'Action/Adventure'
+WHERE GenreID = 1;
+
+
